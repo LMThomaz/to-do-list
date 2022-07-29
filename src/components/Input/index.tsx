8381ef59a3c02ec319a97ac3ca4b8b60d38ea544
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-07-29 13:10:48 -0300

    add placeholder in input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,11 @@
 import styles from './styles.module.css';
 
 export function Input() {
-  return <input className={styles.input} type='text' />;
+  return (
+    <input
+      className={styles.input}
+      type='text'
+      placeholder='Adicione uma nova tarefa'
+    />
+  );
 }
